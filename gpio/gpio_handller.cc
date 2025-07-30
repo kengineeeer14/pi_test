6f44d3f2e1f6592b9d1ce98bfd3ee0edd25094a5
kengineeeer14 <kengineeeer@gmail.com>
2025-07-30 23:30:50 +0900

    パラメータ修正

--- gpio/gpio_handller.cc
+++ gpio/gpio_handller.cc
@@ -4,26 +4,26 @@
 int main(){
     int fd;
 
-    /* GPIO14の操作ファイル生成 */
+    /* GPIO17の操作ファイル生成 */
     fd = open("/sys/class/gpio/export", O_RDWR);
-    write(fd, "14", 2);
+    write(fd, "17", 2);
     close(fd);
     sleep(1);
 
-    /* GPIO14を出力に変更 */
-    fd = open("/sys/class/gpio/gpio14/direction", O_RDWR);
+    /* GPIO17を出力に変更 */
+    fd = open("/sys/class/gpio/gpio17/direction", O_RDWR);
     write(fd, "out", 3);
     close(fd);
 
-    /* GPIO14の出力をON */
-    fd = open("/sys/class/gpio/gpio14/value", O_RDWR);
+    /* GPIO17の出力をON */
+    fd = open("/sys/class/gpio/gpio17/value", O_RDWR);
     write(fd, "1", 1);
     close(fd);
     sleep(2);
 
     /* GPIO14の操作ファイル削除 */
     fd = open("/sys/class/gpio/unexport", O_RDWR);
-    write(fd, "14", 2);
+    write(fd, "17", 2);
     close(fd);
 
     return(0);
