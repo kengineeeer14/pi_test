facb79fb6b50f682f8b26430f577b5567a26ae35
ken <kengineeeer14@gmail.com>
2025-07-30 23:41:33 +0900

    change code to use libgpiod

--- gpio/gpio_handller.cc
+++ gpio/gpio_handller.cc
@@ -1,24 +1,21 @@
-#include <fcntl.h>
-#include <unistd.h>
+#include <gpiod.h>
 #include <iostream>
-#include <string>
-
-void write_to(const std::string& path, const std::string& value) {
-    int fd = open(path.c_str(), O_WRONLY);
-    if (fd < 0) {
-        std::cerr << "Failed to open " << path << std::endl;
-        return;
-    }
-    write(fd, value.c_str(), value.size());
-    close(fd);
-}
+#include <unistd.h>
 
 int main() {
-    write_to("/sys/class/gpio/export", "17");
-    sleep(1);
-    write_to("/sys/class/gpio/gpio17/direction", "out");
-    write_to("/sys/class/gpio/gpio17/value", "1");
+    const char* chipname = "gpiochip0";
+    unsigned int line_num = 17;  // GPIO17
+
+    gpiod_chip* chip = gpiod_chip_open_by_name(chipname);
+    gpiod_line* line = gpiod_chip_get_line(chip, line_num);
+
+    gpiod_line_request_output(line, "gpio_control", 0); // 初期値0 (OFF)
+    gpiod_line_set_value(line, 1); // ON
     sleep(2);
-    write_to("/sys/class/gpio/unexport", "17");
+    gpiod_line_set_value(line, 0); // OFF
+
+    gpiod_line_release(line);
+    gpiod_chip_close(chip);
+
     return 0;
 }
